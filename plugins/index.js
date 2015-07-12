5014939534a7da32e382dc1c5e2dcef936e8c640
Max Kueng <me@maxkueng.com>
2015-07-12 16:43:51 +0200

    log requests to console

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,17 @@ var config = require('../config');
 
 module.exports = [
   {
+    register: require('good'),
+    options: {
+      reporters: [
+        {
+          reporter: require('good-console'),
+          events: { log: '*', response: '*' }
+        }
+      ]
+    }
+  },
+  {
     register: require('./db'),
     options: config
   },
