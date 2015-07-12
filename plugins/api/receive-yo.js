52fc7fd40ce0056b936604daf365a3e37ad76e63
Max Kueng <me@maxkueng.com>
2015-07-12 03:11:05 +0200

    clean

--- plugins/api/receive-yo.js
+++ plugins/api/receive-yo.js
@@ -6,17 +6,10 @@ module.exports = function (request, reply) {
   var username = request.query.username;
   var sendLink = request.server.methods.yo.sendLink;
 
-  console.log('ツ', username);
-
   var feedsLink = 'http://yomypopcorn.com/feed?username=' + username;
-
-  sendLink('MANUELSTOFER', feedsLink, function (err) {
-    console.log('タ', err);
-  });
 
   sendLink(username, feedsLink, function (err) {
     if (err) {
-      console.log(err);
       log.error(err);
 
       return reply(boom.badImplementation());
