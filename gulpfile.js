c0c53df20b78ce42552e4cfb3194614a5ac5f43d
Manuel Stofer <manuelstofer@gmail.com>
2015-07-13 02:02:08 +0200

    Build app.jsx

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var bistre = require('bistre');
 var nodemon = require('gulp-nodemon');
+var jsx = require('gulp-react');
+var babel = require('gulp-babel');
 
 gulp.task('nodemon', function () {
   process.env.NODE_ENV = 'dev';
@@ -22,6 +24,13 @@ gulp.task('nodemon', function () {
       .pipe(bistre({time: true}))
       .pipe(process.stderr);
   });
+});
+
+gulp.task('jsx', function() {
+  return gulp.src('frontend/*.jsx')
+    .pipe(jsx())
+    .pipe(babel())
+    .pipe(gulp.dest('static/js'));
 });
 
 gulp.task('dev', [ 'nodemon' ]);
