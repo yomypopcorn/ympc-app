9a1e62955523820321f61dd9b34f3da3e92426e5
mk <me@maxkueng.com>
2015-07-16 02:04:48 +0200

    bundle app with browserify and all that stuff

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 var colorNames = [
   'turquoise',
   'emerald',
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,84 @@
+process.env.NODE_ENV = 'production';
+
 var gulp = require('gulp');
 var bistre = require('bistre');
 var nodemon = require('gulp-nodemon');
-var babel = require('gulp-babel');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var streamify = require('gulp-streamify');
+var uglify = require('gulp-uglify');
+var babelify = require('babelify');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+
+var isProduction = process.env.NODE_ENV === 'production';
+
+var vendors = [
+  'react'
+];
+
+function handleError (err) {
+  gutil.log(err);
+  process.exit();
+}
+
+gulp.task('build:vendor', function () {
+  var bundler = browserify({
+    debug: !isProduction,
+    require: vendors
+  });
+
+  function rebundle () {
+    gutil.log('vendor rebundle...');
+
+    var stream = bundler.bundle();
+
+    return stream
+      .on('error', function () {
+      })
+      .pipe(source('vendor.js'))
+      .pipe(gulpif(isProduction, streamify(uglify())))
+      .pipe(gulp.dest('static/assets'));
+  }
+
+  return rebundle();
+});
+
+function bundleApp (watch) {
+
+  var bundler = browserify({
+    entries: [ 'frontend/app.jsx' ],
+    debug: !isProduction,
+    extensions: [ '.jsx' ],
+    fullPaths: false
+  });
+
+  bundler.transform(babelify);
+
+  vendors.forEach(function (vendor) {
+    bundler.external(vendor);
+  });
+
+  if (watch) {
+    bundler = watchify(bundler);
+    bundler.on('update', rebundle);
+  }
+
+  function rebundle () {
+    gutil.log('app rebundle...');
+
+    var stream = bundler.bundle();
+
+    return stream
+      .on('error', handleError)
+      .pipe(source('app.js'))
+      .pipe(gulpif(isProduction, streamify(uglify())))
+      .pipe(gulp.dest('static/assets'));
+  }
+
+  return rebundle();
+}
 
 gulp.task('nodemon', function () {
   process.env.NODE_ENV = 'dev';
@@ -10,7 +87,10 @@ gulp.task('nodemon', function () {
     script: 'server.js',
     ext: 'js',
     ignore: [
-      'node_modules/'
+      'node_modules/',
+      'static/',
+      'frontend/',
+      'gulpfile.js'
     ],
     stdout: false,
   })
@@ -25,10 +105,14 @@ gulp.task('nodemon', function () {
   });
 });
 
-gulp.task('jsx', function() {
-  return gulp.src('frontend/*.jsx')
-    .pipe(babel())
-    .pipe(gulp.dest('static/assets'));
+gulp.task('build:app', function () {
+  return bundleApp(false);
 });
 
-gulp.task('dev', [ 'nodemon' ]);
+gulp.task('watch:app', [ 'build:app' ], function () {
+  return bundleApp(true);
+});
+
+gulp.task('build', [ 'build:vendor', 'build:app' ]);
+
+gulp.task('dev', [ 'nodemon', 'watch:app' ]);
