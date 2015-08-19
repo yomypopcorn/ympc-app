e92ba03f12b8c45cffe5045778df4c5e4ab94487
Max Kueng <me@maxkueng.com>
2015-08-19 03:12:47 +0200

    cleanup

--- config.js
+++ config.js
@@ -4,19 +4,6 @@ var defaults = {
   port: 9987,
   enableCors: true,
   uri: 'http://localhost:9987',
-  yoApiKey: 'yolo',
-  redisSocket: null,
-	redisHost: '127.0.0.1',
-	redisPort: 6379,
-	redisPassword: null,
-};
-
-var defaults = {
-  logLevel: 'info',
-  address: '0.0.0.0',
-  port: 9987,
-  enableCors: true,
-  uri: 'http://localhost:9987',
   yo: {
     apikey: 'yolo'
   },
